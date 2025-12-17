8cfec2558ff4c3bf0692ea1943e635e1bbfb0127
Hitori <165667417+Hynf5@users.noreply.github.com>
2025-12-17 21:48:17 +0700

    Create SekaiKomikLat.kt
    Add Source "sekaikomik.lat"

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/SekaiKomikLat.kt
@@ -0,0 +1,17 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.id
+
+import org.koitharu.kotatsu.parsers.Broken
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("SEKAIKOMIKLAT", "SekaiKomikLat", "id")
+internal class WarungKomik(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaParserSource.WARUNGKOMIK, "www.sekaikomik.lat", pageSize = 20, searchPageSize = 10) {
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = super.filterCapabilities.copy(
+			isTagsExclusionSupported = false,
+		)
+  }
